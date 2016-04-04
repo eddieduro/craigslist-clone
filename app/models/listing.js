77dcdf250796bb3562a10de56d688ea5a61ce6e0
eddie duro and erik tolentino <erik@>
2016-04-04 09:43:09 -0700

    upload init files

---
+++ app/models/listing.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  date: DS.attr(),
+  content: DS.attr(),
+  author: DS.attr(),
+  location: DS.attr(),
+  compensation: DS.attr(),
+  category: DS.belongsTo('category', {async:true})
+});
